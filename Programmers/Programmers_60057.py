70b467d65cafc5426e46948cb5314d5cd5139483
minocrafft <minocrafft@gmail.com>
2022-06-16 23:55:11 +0900

    update programmers_60057

--- Programmers/Programmers_60057.py
+++ Programmers/Programmers_60057.py
@@ -18,17 +18,34 @@ import re
 
 
 def solution(string):
+    print(string)
+    answer = []
     for step in range(1, len(string)):
-        print(string)
         split_string = [string[i:i + step] for i in range(0, len(string), step)]
-        print(f"{step}-th")
         print(f"new_string is {split_string}")
-        counter_string = Counter(split_string)
-        for k in counter_string.keys():
-            print(f"Key for Counter_string: {k}")
-            if k in split_string:
-                new_string = re.sub(f'{k}+', f'{k}', string)
-                print(f"new_string: {new_string}")
+        new_string = ''
+        i, j, cnt = 0, 0, 0
+        for s in range(len(split_string)):
+            if split_string[s] == split_string[i]:
+                cnt += 1
+            else:
+                if cnt == 1:
+                    new_string += split_string[i]
+                    continue
+                new_string += str(cnt) + split_string[i]
+                cnt = 1
+                i = s
+        answer.append(new_string)
+
+    print(answer)
+
+
+        # counter_string = Counter(split_string)
+        # for k in counter_string.keys():
+        #     print(f"Key for Counter_string: {k}")
+        #     if k in split_string:
+        #         new_string = re.sub(f'{k}+', f'{k}', string)
+        #         print(f"new_string: {new_string}")
         # new_string = re.replace(f'{Counter_string}')
 
         # print(f"{dir(re.__doc__)}")
