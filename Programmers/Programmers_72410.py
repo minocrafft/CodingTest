5e13410b59737f803e0aaa5d38c5f89efa0c59bd
minocrafft <minocrafft@gmail.com>
2022-06-19 00:25:31 +0900

    Add Programmers_72410.py file

--- Programmers/Programmers_72410.py
+++ Programmers/Programmers_72410.py
@@ -29,3 +29,11 @@ def solution(new_id):
             new_id += new_id[-1]
 
     return new_id
+
+def solution(new_id):
+    new_id = re.sub('[^a-z0-9\-_.]', '', new_id.lower())
+    new_id = re.sub('[.]{2,}', '.', new_id).strip('.')
+    new_id = 'a' if not new_id else new_id[:15].strip('.')
+    if len(new_id) < 3:
+        new_id += ''.join([new_id[-1] for _ in range(3 - len(new_id))])
+    return new_id
