e99b208b2982ed1c5ff7bda1962f24638d8636f2
minocrafft <minocrafft@gmail.com>
2022-06-19 00:28:14 +0900

    update 72410

--- Programmers/Programmers_72410.py
+++ Programmers/Programmers_72410.py
@@ -20,15 +20,6 @@
 """
 
 import re
-def solution(new_id):
-    new_id = re.sub('[^a-z0-9-_.]', '', new_id.lower())
-    new_id = re.sub('[.]+', '.', new_id).strip('.')
-    new_id = 'a' if new_id == '' else new_id[:15].strip('.')
-    if len(new_id) <= 2:
-        while len(new_id) < 3:
-            new_id += new_id[-1]
-
-    return new_id
 
 def solution(new_id):
     new_id = re.sub('[^a-z0-9\-_.]', '', new_id.lower())
